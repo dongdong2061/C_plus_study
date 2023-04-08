3688583449dca30e567d5a8793f598a604111899
dongding <2043234641@qq.com>
2023-04-08 18:23:12 +0800

    deleted:    01.exe 	new file:   2_2.cpp

---
+++ 2_2.cpp
@@ -0,0 +1,21 @@
+//   my first C++ code
+//   data:2023.04.03
+#include <iostream>
+
+int main(void)
+{
+    using namespace std;
+
+    /*cout << "Come up and c++ me some time.";
+    cout << endl;
+    cout << "You won't regret it." << endl;
+    return 0; */
+
+    int carrots = 25;
+    cout << "I have ";
+    cout << carrots;
+    cout << endl;
+    
+    return 0;
+
+}
